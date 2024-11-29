6b0b31a48d70ac15b910472a256d7fbe661b9475
Soufiane Majdar <soufiane.majdar@gmail.com>
2024-11-30 00:55:50 +0100

    new update

--- trips/urls.py
+++ trips/urls.py
@@ -5,6 +5,7 @@ app_name = 'trips'
 
 urlpatterns = [
     path('', views.trip_list, name='trip-list'),
+    path('search/', views.search, name='search'),
     path('<slug:slug>/', views.trip_detail, name='trip-detail'),
     path('category/<str:trip_type>/', views.trip_category, name='trip-category'),
     path('inquiry/submit/', views.submit_inquiry, name='submit-inquiry'),
