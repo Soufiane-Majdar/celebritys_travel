88b4a5af626680899662161590347e891f74a715
Soufiane Majdar <soufiane.majdar@gmail.com>
2024-12-05 16:22:37 +0100

    vercel config

---
+++ build_files.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# # Create virtual environment
+# python -m venv venv
+# source venv/bin/activate
+
+# Install dependencies
+pip3.12 install -r requirements.txt
+
+# Collect static files
+# python3.12  manage.py collectstatic --noinput
